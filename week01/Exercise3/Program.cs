a5904dd0bdce5324b8f4e9b928b94d91cb2a2df2
denlop-gh <estuardo.lpz@outlook.com>
2025-07-08 19:48:57 -0600

    exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int input = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != input)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (input > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (input < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations, you guessed the magic number!");
+            }
+        }
     }
 }
